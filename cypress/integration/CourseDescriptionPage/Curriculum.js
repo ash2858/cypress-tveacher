f30c95e1d9fc977bb28e367e04713934dd110f9b
adarsh2858 <adarsh2858@gmail.com>
2021-03-16 15:42:04 +0530

    verify the contents and their attributes present on the course overview page for a student

--- cypress/integration/CourseDescriptionPage/Curriculum.js
+++ cypress/integration/CourseDescriptionPage/Curriculum.js
@@ -9,19 +9,35 @@ describe("Verify the chapters on the course overview page curriculum", () => {
     Cypress.Cookies.preserveOnce("_cloudyuga_session");
   });
 
-  it("searches for the course", () => {
+  it("searches for the course and clicks on the course card", () => {
     cy.visit(explorePath);
 
     cy.get("#all-courses").scrollIntoView().wait(1000);
 
     cy.get("input[placeholder='Search Courses']").type("test cypress 3");
+
+    cy.contains("Test Cypress 3").scrollIntoView().wait(1000).click();
   });
 
-  it("clicks on the course card", () => {
-    cy.contains("Test Cypress 3").scrollIntoView().wait(1000).click();
-    cy.url().should("include", "/courses/test-cypress-3");
+  it("verifies the sections present on the course overview page", () => {
     cy.contains("Course Curriculum").scrollIntoView().wait(500);
     cy.get(".course-accordion").contains("Two Chapters Section");
     cy.get(".course-accordion").contains("Four Chapters Section");
   });
+
+  it("verifies the chapters present on the curriculum", ()=>{
+    cy.get(':nth-child(1) > a > p > .font-bold').contains("Enabled Preview and Published Chapter")
+    cy.get(':nth-child(2) > a > p > .font-bold').contains("Enabled Preview and Published Chapter")
+
+    cy.get(".course-accordion").contains("Two Chapters Section").click();
+    cy.get(".course-accordion").contains("Four Chapters Section").click();
+
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(1) > a > p > .font-bold').contains("Three Lessons Chapter")
+    cy.get(':nth-child(2) > :nth-child(2) > :nth-child(2) > a > p > .font-bold').contains("No Preview Published Chapter")
+  })
+
+  it("checks for the lock and unlocked icons", ()=>{
+    cy.get(':nth-child(1) > a > p > .inline-block').should("have.attr", "src", "/packs/images/lock_open-e0513700011dae902f73a4b60fd8ae19.svg")
+    cy.get(':nth-child(2) > a > p > .inline-block').should("have.attr", "src", "/packs/images/icon_lock-d9561e6b1d46b93be9e68bdc5c5afb71.svg")
+  })
 });
